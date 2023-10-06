eabd12afd548f33ffcf069e6737de53a3c7757fa
C.A.Tucker <cartheur@pm.me>
2023-10-06 18:18:15 +0200

    Initial version

---
+++ Smdn.Devices.MCP2221/DeviceUnavailableException.cs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+
+using System;
+
+using Smdn.Devices.UsbHid;
+
+namespace Smdn.Devices.MCP2221;
+
+internal class DeviceUnavailableException : UnauthorizedAccessException {
+  public DeviceUnavailableException(Exception innerException, IUsbHidDevice device = null)
+    : base($"MCP2221/MCP2221A is not available, not privileged or disconnected. (device='{device?.FileSystemName ?? device?.DevicePath ?? "?"}')", innerException)
+  {
+  }
+}
